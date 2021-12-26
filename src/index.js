7fd6a2ccb6ccda163afb31931046e3a2e025df95
Taylor Noj <taylornoj@gmail.com>
2021-12-25 21:55:25 -0500

    defined handleClick

--- src/index.js
+++ src/index.js
@@ -3,19 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {   // need to always call super when defining constructor of a subclass
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
   render() {
     return (
-      <button className="square" 
-      onClick={() =>
-        this.setState({value: 'X'})  // we are telling React to re-render the square whenever <button> is clicked and now we will see 'X' upon click
-        }>
-        {this.state.value}    
+      <button className="square"
+        onClick={() => this.props.onClick()}
+        >
+        {this.props.value}
       </button>
     );
   }
@@ -28,8 +21,20 @@ class Board extends React.Component {
       squares: Array(9).fill(null),
     };
   }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+  
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
