87250fc2d0ede88a0ef8fd7136031d727532c0dc
Taylor Noj <taylornoj@gmail.com>
2021-12-25 22:57:48 -0500

    edit history so that if we go to previous play, future history is now incorrect and plays are removed

--- src/index.js
+++ src/index.js
@@ -56,7 +56,8 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0,
+      this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -67,6 +68,7 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
