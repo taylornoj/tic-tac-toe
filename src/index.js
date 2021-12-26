36a70b2d7d77d0eb4ba6d45616456823f1b55376
Taylor Noj <taylornoj@gmail.com>
2021-12-25 22:14:06 -0500

    add xIsNext and update boards handleClick to flip value to start showing O's

--- src/index.js
+++ src/index.js
@@ -15,13 +15,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,    // each time a player moves, this will be flipped to determine which player goes next
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
